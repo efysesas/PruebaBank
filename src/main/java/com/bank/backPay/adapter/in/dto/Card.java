1d55c71baefd803b740ff0caf2d8ba44fdcc4a71
efyse <brayan.garzon.porras@gmail.com>
2024-11-18 11:25:13 -0500

    * Se agregan validadores para parametros de entrada * se habilita manejo de errores

--- src/main/java/com/bank/backPay/adapter/in/dto/Card.java
+++ src/main/java/com/bank/backPay/adapter/in/dto/Card.java
@@ -10,14 +10,13 @@ import lombok.Data;
 @Data
 public class Card {
 	@NotNull(message = "El ID no puede ser nulo")
-	@Size(max = 16, message = "El cardId debe tener 16 caracteres")
+	@Size(min = 16, max = 16, message = "El cardId debe tener 16 caracteres")
 	private String cardId;
 	private double balance;
 	private boolean active;
 	@Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
 	private String firstName;
 	private String firstLastName;
-	@Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "La fecha debe tener el formato yyyy-MM-dd")
 	private Date expirationDate;
 	private boolean blocked;
 }
